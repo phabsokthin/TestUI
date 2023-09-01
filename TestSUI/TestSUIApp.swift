943a42f2d9b00f935e9e5f0102b21de7007f8625
macos <Phabsokthin123@gmail.com>
2023-09-01 11:15:51 +0700

    Initial Commit

---
+++ TestSUI/TestSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSUIApp.swift
+//  TestSUI
+//
+//  Created by MacOS on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct TestSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
